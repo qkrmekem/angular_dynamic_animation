751cf42fbb9b7068bcb4cbdf091a981c191f6e9d
qkrmekem <magicofclown@naver.com>
2024-07-12 18:00:33 +0900

    json으로 애니메이션 적용시키기

--- src/app/classes/animations.factory.ts
+++ src/app/classes/animations.factory.ts
@@ -1,4 +1,4 @@
-import { animate, AnimationBuilder, AnimationStyleMetadata } from "@angular/animations";
+import { animate, AnimationBuilder, AnimationStyleMetadata, style } from "@angular/animations";
 import { inject } from "@angular/core";
 import { CustomAnimationDefinition } from "../reusable-animation-test/reusable-animation-test.component";
 
@@ -6,7 +6,11 @@ import { CustomAnimationDefinition } from "../reusable-animation-test/reusable-a
 export class AnymationCreateFactory{
     builder = inject(AnimationBuilder);
 
-    createAnimation(parma: CustomAnimationDefinition){
-        return this.builder.build([animate(parma.time, parma.style)]);
+    createAnimation(param: CustomAnimationDefinition){
+        console.log('파람',param.style);
+        const result = this.builder.build([animate(`${param.time}s`, style(param.style))]);
+        console.log('빌드', result);
+        
+        return result;
     }
 }
--- src/app/reusable-animation-test/reusable-animation-test.component.ts
+++ src/app/reusable-animation-test/reusable-animation-test.component.ts
@@ -1,11 +1,13 @@
 import { AnimationBuilder, AnimationStyleMetadata, style } from '@angular/animations';
-import { Component, ElementRef, inject, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { AnymationCreateFactory } from '../classes/animations.factory';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 type AnimationKeys = 'open' | 'close';
 
 export interface CustomAnimationDefinition{
-  style: AnimationStyleMetadata;
+  style: any;
   time: string;
 }
 
@@ -37,7 +39,19 @@ export const animationCustomDefinition: Record<AnimationKeys, CustomAnimationDef
   templateUrl: './reusable-animation-test.component.html',
   styleUrl: './reusable-animation-test.component.css'
 })
-export class ReusableAnimationTestComponent{
+export class ReusableAnimationTestComponent implements OnInit{
+  http = inject(HttpClient);
+  async ngOnInit(): Promise<void> {
+    this.keys = await firstValueFrom(this.http.get('./assets/json/animation.data.json'))
+    console.log(this.keys);
+    this.animationDefinition = (await firstValueFrom(this.http.get('./assets/json/animation.data.json')) as any).animation_definition;
+    console.log(this.animationDefinition);
+    
+  }
+
+  keys = {} as any;
+  animationDefinition: Record<AnimationKeys, any> = {} as Record<AnimationKeys, any>;
+
 
   @ViewChild('animationTarget', {read: ElementRef, static: true}) target!: ElementRef;
 
@@ -48,10 +62,13 @@ export class ReusableAnimationTestComponent{
 
   isOpen = false;
   openStatus: AnimationKeys = 'close';
+  
 
   toggle(){
     // const end = animationCustomDefinition[this.isOpen ? 'close' : 'open'];
-    const aniFactory = this.factory.createAnimation(animationCustomDefinition[this.openStatus]);
+    const aniFactory = this.factory.createAnimation(this.animationDefinition[this.openStatus]);
+    console.log('팩토리', aniFactory);
+    
     const player = aniFactory.create(this.target.nativeElement);
     player.play();
     this.isOpen = !this.isOpen;
